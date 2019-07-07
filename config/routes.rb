b819642b3d52a5e0221945c94d4d8d61d17016a8
Caio <csc.snf18@uea.edu.br>
2019-07-07 16:47:21 -0400

    Final

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   get 'map', to: "maps#index"
   get 'infecteds', to: 'survivors#infected_locations' # ?lat=latitude&lng=longitude
   get 'infected_survivors', to: 'details#infected_survivors'
-  get 'not_infected_survivors', to: 'details# not_infected_survivors'
+  get 'not_infected_survivors', to: 'details#not_infected_survivors'
   get 'average_resources', to: 'details#average_resources_by_survivor'
   get 'points_lost', to: 'details#points_lost_due_to_infected'
   post 'trades', to: 'trades#create'
