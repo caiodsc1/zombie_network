54ba22e555eeddb3125b2bacbcfe9fa0225313fa
Caio <csc.snf18@uea.edu.br>
2019-07-07 16:42:33 -0400

    Final

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,13 @@
 Rails.application.routes.draw do
   get 'map', to: "maps#index"
   get 'infecteds', to: 'survivors#infected_locations' # ?lat=latitude&lng=longitude
+  get 'infected_survivors', to: 'details#infected_survivors'
+  get 'not_infected_survivors', to: 'details# not_infected_survivors'
+  get 'average_resources', to: 'details#average_resources_by_survivor'
+  get 'points_lost', to: 'details#points_lost_due_to_infected'
   post 'trades', to: 'trades#create'
   post 'report_infection', to: 'infections#create'
   patch 'survivors/:id', to: 'survivors#update'
   resources :survivors, except: :update # disable PUT
+  root 'maps#index'
 end
