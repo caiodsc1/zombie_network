f654c08d5b22e93d302838973b3cfbe01b89e988
Caio <csc.snf18@uea.edu.br>
2019-07-07 16:57:21 -0400

    Final

--- app/controllers/trades_controller.rb
+++ app/controllers/trades_controller.rb
@@ -14,8 +14,8 @@ class TradesController < ApplicationController
   private
 
   def set_survivors_and_resources
-    @survivor_one = Survivor.find(params[:survivor_one]).includes(:inventory)
-    @survivor_two = Survivor.find(params[:survivor_two]).includes(:inventory)
+    @survivor_one = Survivor.includes(:inventory).find(params[:survivor_one])
+    @survivor_two = Survivor.includes(:inventory).find(params[:survivor_two])
     @resources_one = params.require(:resources_one).permit(:water, :food, :medication, :ammunition)
     @resources_two = params.require(:resources_two).permit(:water, :food, :medication, :ammunition)
   end
